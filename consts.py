68df3adc610aa40eb58de58b0d710d5f71948745
kim <kimkes25@gmail.com>
2022-03-21 18:42:55 +0200

    path

--- consts.py
+++ consts.py
@@ -1,8 +1,8 @@
 edges_files = [
-    'C:\\Users\\kimke\\OneDrive\\Documents\\4th year\\semeter B\\Biological networks sadna\\network-analysis\\Benchmarks\\Yeast\\edges.txt',
-    'C:\\Users\\kimke\\OneDrive\\Documents\\4th year\\semeter B\\Biological networks sadna\\network-analysis\\Benchmarks\\Arabidopsis\\edges.txt',
+    'Benchmarks\\Yeast\\edges.txt',
+    'Benchmarks\\Arabidopsis\\edges.txt',
 ]
 clusters_files = [
-    'C:\\Users\\kimke\\OneDrive\\Documents\\4th year\\semeter B\\Biological networks sadna\\network-analysis\\Benchmarks\\Yeast\\clusters.txt',
-    'C:\\Users\\kimke\\OneDrive\\Documents\\4th year\\semeter B\\Biological networks sadna\\network-analysis\\Benchmarks\\Arabidopsis\\clusters.txt',
+    'Benchmarks\\Yeast\\clusters.txt',
+    'Benchmarks\\Arabidopsis\\clusters.txt',
 ]
