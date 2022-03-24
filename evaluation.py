8e0594f5e6a373f430ea59104c19eca3bd24f42d
kim <kimkes25@gmail.com>
2022-03-24 11:59:45 +0200

    evaluation funcs

---
+++ evaluation.py
@@ -0,0 +1,159 @@
+from algorithms import louvain, newman
+from input_networks import *
+import math
+
+
+# Helpers
+def calc_jaccard(clusters_dict1, clusters_dict2):
+    """
+    calculates jaccard score between clusters_dict1 and clusters_dict2
+    :param clusters_dict1, clusters_dict2: dictionary where the key is the cluster's index
+           and the values are the points indexes that are members of the cluster
+    :return: the jaccard score
+    """
+    couples1 = all_possible_couples(clusters_dict1)
+    couples2 = all_possible_couples(clusters_dict2)
+    intersection = intersection_size(couples1, couples2)
+    union = len(couples1) + len(couples2) - intersection
+    return intersection / union
+
+
+def intersection_size(set1, set2):
+    """
+    :return: how many items intersects between set1 and set2
+    """
+    intersection_size = 0
+    # checking which set is smaller in order to go over it efficiently
+    size1 = len(set1)
+    size2 = len(set2)
+    shorter_set = set1 if size1 < size2 else set2
+    longer_set = set2 if size1 < size2 else set1
+
+    for item in shorter_set:
+        if item in longer_set:
+            intersection_size += 1
+
+    return intersection_size
+
+
+def all_possible_couples(clusters_dict):
+    """
+    :return: return all possible couples whithin points that are in the same cluster, for every cluster
+             for example, if we have 2 clusters and the dict looks like: {1: [2,3], 2: [4,5,6]}
+             the function returns a set: {(2,3),(4,5),(5,6),(4,6)}
+    """
+    all_possible_couples_set = set()
+    for cluster in clusters_dict.values():
+        all_possible_couples_set.update(combinations(cluster))
+    return all_possible_couples_set
+
+
+def combinations(lst):
+    """
+    :return: this func returns all n choose 2 combinations of items in list
+    """
+    couples_set = set()
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            couples_set.add(tuple(sorted([lst[i], lst[j]])))
+    return couples_set
+
+
+def create_clusters_dict(communities_list):
+    '''
+    :param communities_list:
+    :return: a dictionary with number as key and cluster members in a list as a value
+    '''
+    curr_dict = dict()
+    i = 0
+    for community in communities_list:
+        curr_dict[i] = list(community)
+        i += 1
+    return curr_dict
+
+
+# Internal
+def modularity(G, communities):
+    return nx.algorithms.community.modularity(G, communities)
+
+
+# TODO make sure it is the same as in requiremants
+# https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.cuts.conductance.html
+def conductance(G, cluster):
+    '''
+    measurement for each cluster - how many edges are in the cluster vs. put of it
+    :return:
+    '''
+    return nx.conductance(G, cluster)
+
+
+def graph_conductance(G, communities):
+    '''
+    :param G:
+    :param communities:
+    :return: conductance score of a graph
+    '''
+    res = sum([conductance(G, cluster) for cluster in communities])
+    return 1 - (1 / len(communities) * res)
+
+
+# External
+def jaccard(communities1, communities2):
+    clusters_dict1 = create_clusters_dict(communities1)
+    clusters_dict2 = create_clusters_dict(communities2)
+    return calc_jaccard(clusters_dict1, clusters_dict2)
+
+
+def known_community_sensitivity(known_community, candidate_communities):
+    max_intersection = 0
+    for candidate_community in candidate_communities:
+        max_intersection = max(intersection_size(known_community, candidate_community), max_intersection)
+    return max_intersection
+
+
+def graph_sensitivity(known_communities, candidate_communities):
+    I = 0
+    N = 0
+    for knwon_community in known_communities:
+        N += len(knwon_community)
+        I += known_community_sensitivity(knwon_community, candidate_communities)
+    return I / N
+
+
+# TODO make more efficient and make sure is right
+def PPV_max_per_community(candidate_community, known_communities):
+    max_res = 0
+    for known_community in known_communities:
+        int_with_current = intersection_size(candidate_community, known_community)
+        max_res = max(max_res, int_with_current)
+    return max_res
+
+
+def graph_PPV(known_communities, candidate_communities):
+    PPV = 0
+    int_all_total = 0
+    for candidate_community in candidate_communities:
+        int_with_all = sum([intersection_size(candidate_community, com) for com in known_communities])
+        int_all_total += int_with_all
+        PPV += PPV_max_per_community(candidate_community, known_communities)
+    return PPV / int_all_total
+
+
+def graph_accuracy(known_communities, candidate_communities):
+    ppv = graph_PPV(known_communities, candidate_communities)
+    sn = graph_sensitivity(known_communities, candidate_communities)
+    return math.sqrt(sn * ppv)
+
+
+G = create_random_network(250, 0.1, 3, 1.5, 5, 20)
+communities_louvain = louvain(G)
+communities_newman = newman(G)
+real_communities1 = {frozenset(G.nodes[v]["community"]) for v in G}
+# print(graph_conductance(G,communities_louvain))
+print("jaccard is newman: ", jaccard(communities_newman, real_communities1))
+print("graph_sensitivity with newman is: ", graph_sensitivity(real_communities1, communities_newman))
+print("graph_accuracy with newman is: ", graph_accuracy(real_communities1, communities_newman))
+
+print("jaccard is lovauin: ", jaccard(communities_louvain, real_communities1))
+print("graph_sensitivity with louvain is: ", graph_sensitivity(real_communities1, communities_louvain))
+print("graph_accuracy with louvain is: ", graph_accuracy(real_communities1, communities_louvain))
