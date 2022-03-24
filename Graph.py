b72c64a66f9a608850a0611015dde22b11736d6e
Esty Katzeff <esty.katzeff@gmail.com>
2022-03-24 12:18:20 +0200

    graph object for all to use

---
+++ Graph.py
@@ -0,0 +1,57 @@
+
+class Graph:
+    def __init__(self, edges_file):
+        self.nodes_range = None  # create_edges_list() updates nodes_range to be node with highest value
+        self.edges_file = edges_file
+        self.edges_list = self.create_edges_list()
+        self.adj_matrix = self.create_adj_matrix()
+        self.degree_list = self.create_degree_list()  # maybe remove this from init bc heavy
+
+    """
+    a_ij = 1     if (i,j) in E
+    a_ij = 0     else
+    
+    Assumption: nodes_range has been updated by create_edges_list()    
+    """
+    # TODO: in future - implement more space-efficiently (adjacency list?)
+    def create_adj_matrix(self):
+        adj_mat = [[0]*self.nodes_range for _ in range(self.nodes_range)]  # Initialize adjacency matrix
+        for i, j in self.edges_list:
+            adj_mat[i-1][j-1] = 1
+            adj_mat[j-1][i-1] = 1  # make sure it is undirected graph
+        return adj_mat
+
+    def print_adj_matrix(self):
+        for i in range(len(self.adj_matrix)):
+            for j in range(len(self.adj_matrix)):
+                print(self.adj_matrix[i][j], " ", end='')
+            print('')
+
+    # degree of node is calculated by: d_i = sum(a_il)   0<=l<=n
+    def create_degree_list(self):
+        degree_list = [0] * self.nodes_range  # initializing node degrees list
+        for i, j in self.edges_list:
+            degree_list[i - 1] += 1
+            if i != j:  # not an edge to itself
+                degree_list[j - 1] += 1  # Assumption: G is undirected graph - if there is (i,j) then there isn't (j,i)
+        return degree_list
+
+    def create_edges_list(self):
+        max_node_val = 0
+        edges = []
+        with open(self.edges_file) as file:
+            while line := file.readline():
+                edge = tuple(line.rstrip().split())
+                edge = tuple((int(edge[0]), int(edge[1])))
+                edges.append(edge)
+                max_node_val = max(max_node_val, edge[0], edge[1])
+        self.nodes_range = max_node_val
+        return edges
+
+
+
+
+#G = Graph("LFRBenchmark/Graphs/1000_0.4_0/network.dat")
+# G = Graph("C:/Users/97252/Documents/year_4/sadna/tests/network.dat")
+# G.print_adj_matrix()
+# print(G.degree_list)
