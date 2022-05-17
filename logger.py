da736ad6f8e80cf824dccb9131a9c811d1eefca6
kim <kimkes25@gmail.com>
2022-05-17 10:22:15 +0300

    logger time format

--- logger.py
+++ logger.py
@@ -111,17 +111,10 @@ def setup_logger(file_name):
     # Setup logging
     if (not setup_logging(console_log_output="stdout", console_log_level="debug", console_log_color=True,
                           logfile_file=file_name + ".log", logfile_log_level="debug", logfile_log_color=False,
-                          log_line_template="%(color_on)s[%(created)d] [%(threadName)s] [%(levelname)-8s] %(message)s%(color_off)s")):
+                          log_line_template="%(color_on)s[%(asctime)s] [%(threadName)s] [%(levelname)-8s] %(message)s%(color_off)s")):
         print("Failed to setup logging, aborting.")
         return 1
     print(f"******logging to {file_name}.log********")
-
-    # Log some messages
-    # logging.debug("Debug message")
-    # logging.info("Info message")
-    # logging.warning("Warning message")
-    # logging.error("Error message")
-    # logging.critical("Critical message")
 
 
 # Call main function
